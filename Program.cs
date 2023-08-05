fc4b1a660718ffe38d5d4de55262c5a3ad17aca6
Manaswini Ray <106983825+manaswiniray@users.noreply.github.com>
2023-08-05 13:16:56 +0530

    updated by Manaswiniray

--- Program.cs
+++ Program.cs
@@ -9,10 +9,11 @@
             int j = 0;
             for (int i = 0; i < m; i++)
             {
+                j++;
                 sum += i;
             }
 
-            Console.WriteLine("Hello, my value is {0}!", sum);
+            Console.WriteLine("Hello, my value is {0} and j is {1}!", sum,j);
         }
     }
 }
