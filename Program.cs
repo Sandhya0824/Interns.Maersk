a4eb5d829a562a669c071506ce7e6873ddcd3de2
Manaswini Ray <106983825+manaswiniray@users.noreply.github.com>
2023-08-05 12:30:23 +0530

    upload .cs .csproj

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Interns.Maersk.DemoProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = 10;
+            int sum = 0;
+
+            for (int i = 0; i < m; i++)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine("Hello, my value is {0}!", sum);
+        }
+    }
+}
